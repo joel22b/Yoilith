a6e982641363aa082539ca2329b0649f3fb59b34
brauj1894 <brauj1894@wrdsb.ca>
2018-09-27 13:35:55 -0400

    Version 2.6 Created Class, to be expanded

---
+++ core/src/com/icsgame/game/map/ItemInteractive.java
@@ -0,0 +1,11 @@
+package com.icsgame.game.map;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.math.Vector2;
+
+public class ItemInteractive extends Item {
+
+    public ItemInteractive(MapMain map, Texture txt, Vector2 vPos, int nW, int nH){
+        super(map, txt, vPos, nW, nH);
+    }
+}
